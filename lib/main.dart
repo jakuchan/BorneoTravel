b47bb2407692438cc2fa803eb41ad68c1f51f02d
jakuchan <543221045@smktelkom-bjb.sch.id>
2024-05-28 07:52:04 +0800

    wisatalagi

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Borneo Travel',
+      title: 'Borneo travels',
       debugShowCheckedModeBanner: false,
       home: AppGua(),
       theme: ThemeData(
